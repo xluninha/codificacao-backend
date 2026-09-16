85be0353abe368cfb22198039ec2a90bb8420d72
Camila Pinheiro <camilafofinha012@gmail.com>
2026-09-16 16:21:10 -0300

    fix: move arquivos da aula 07 para a subpasta correta

--- aula07-fundamentos-nestjs/aula02-modulos-commonsjs-esm/utils.js
+++
@@ -1,9 +0,0 @@
-export function formatLog(mensagem){
-    const dataAtual = new Date()
-    .toISOString().split('T')[0]
-
-    const horaAtual = new Date()
-    .toLocaleTimeString();
-
-    return(`[${dataAtual} - ${horaAtual}]: ${mensagem}`);
-}
